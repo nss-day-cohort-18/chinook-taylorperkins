44329d051690bad9252c7f7fb413d7eed0f925a5
Taylor Perkinse <perkinstaylor.luthier@gmail.com>
2017-04-27 11:39:53 -0500

    Finished sql statement 1

---
+++ non_usa_customers.sql
@@ -0,0 +1,10 @@
+/*
+ non_usa_customers.sql: 
+	Provide a query showing Customers 
+	(just their full names, customer ID and country)
+	who are not in the US.
+*/
+
+SELECT c.FirstName, c.LastName, c.CustomerId, c.Country
+FROM Customer c
+WHERE c.Country IS NOT 'USA'
